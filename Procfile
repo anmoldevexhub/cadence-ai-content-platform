81a1015a94fa7bdaa23fc76fe3e7bf76a6c72cff
Anmolkumar <anmolkumar@devexhub.in>
2026-07-09 16:25:33 +0530

    update requirements.txt file

---
+++ Procfile
@@ -0,0 +1,3 @@
+web: gunicorn cadence_project.wsgi --log-file -
+worker: celery -A cadence_project worker -l info
+beat: celery -A cadence_project beat -l info
--- cadence_project/settings.py
+++ cadence_project/settings.py
@@ -59,6 +59,7 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -134,8 +135,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 
-STATICFILES_DIRS = [BASE_DIR / 'frontend']
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'frontend']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # -----------------------------------------------------------------
 # Email
